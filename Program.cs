580fcf68a1353d005207ec42faea091a96a5277e
Nara19941008 <narminim@code.edu.az>
2022-07-19 00:59:41 +0400

    Added bio table

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper.WriteConsole(ConsoleColor.Blue, "select the option");
+            
+            Helper.WriteConsole(ConsoleColor.Yellow,"1- Create library,2-Get libfrary by id,3-Update library,4-Delete library");
+
+            while (true)
+            {
+                string selectOption = Console.ReadLine();
+                Console.WriteLine(selectOption);
+            }
+        }
+    }
+}
